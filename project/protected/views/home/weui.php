1da51b0b9efb02275b4d510380368bb67bfe989d
sibenx <jsjscool@outlook.com>
2016-03-21 21:02:05 +0800

    添加col

--- project/protected/views/home/weui.php
+++ project/protected/views/home/weui.php
@@ -5,12 +5,12 @@
 
         <p class="weui_msg_desc">
             <img width="160" height="160" style="border-radius:80px;width: 160px;height: 160px;"
-                 src="http://shp.qpic.cn/weixinsrc_pic/pScBR7sbqjOBJomcuvVJ6iacVrbMJaoJZkFUIq4nzQZUIqzTKziam7ibg/">
+                 src="/bscy/project/public/img/panda.png">
         </p><br>
         <h4>擂主：熊方磊 <br>  工作地址：春熙路</h4>
         <br>
-        <p class="weui_msg_desc">
-            文字视图显示大段文字，这些文字通常是页面上的主体内容。Article 支持分段、多层标题、引用、内嵌图片、有/无序列表等富文本样式，并可响应用户的选择操作。
+        <p class="weui_msg_desc" style="text-align: left;">
+            《功夫熊猫》是一部以中国功夫为主题的美国动作喜剧电影，影片以中国古代为背景，其景观、布景、服装以至食物均充满中国元素。故事讲述了一只笨拙的熊猫立志成为武林高手的故事。该片由约翰·斯蒂芬森和马克·奥斯本执导，梅丽·莎科布制片。杰克·布莱克、成龙、达斯汀·霍夫曼、安吉丽娜·朱莉、刘玉玲、塞斯·罗根、大卫·克罗素和伊恩·麦西恩等配音。
         </p>
 
     </div>
