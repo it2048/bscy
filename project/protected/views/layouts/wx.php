5ae68a0d1e87ed5de16e3c817d347ec2c40e6ffc
sibenx <jsjscool@outlook.com>
2016-04-01 08:45:12 +0800

    添加col

--- project/protected/views/layouts/wx.php
+++ project/protected/views/layouts/wx.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="//res.wx.qq.com/open/libs/weui/0.4.0/weui.css"/>
-    <link rel="stylesheet" href="<?=Yii::app()->request->baseUrl."/public/home/css/lyb.css?v=335";?>"/>
+    <link rel="stylesheet" href="<?=Yii::app()->request->baseUrl."/public/home/css/lyb.css?v=336";?>"/>
     <link rel="stylesheet" href="<?=Yii::app()->request->baseUrl."/public/home/frozen/css/frozen.css";?>"/>
     <script type="text/javascript" src="//lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js?v6.0"></script>
     <title>擂台赛</title>
