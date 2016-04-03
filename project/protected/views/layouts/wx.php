ef0e96f2028b7459c922387f8a179b49f3556609
sibenx <jsjscool@outlook.com>
2016-04-03 11:17:44 +0800

    添加百度统计

--- project/protected/views/layouts/wx.php
+++ project/protected/views/layouts/wx.php
@@ -9,6 +9,15 @@
     <script type="text/javascript" src="//lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js?v6.0"></script>
 <!--    <script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>-->
     <title>2016冠军挑战赛-成都市场</title>
+    <script>
+        var _hmt = _hmt || [];
+        (function() {
+            var hm = document.createElement("script");
+            hm.src = "//hm.baidu.com/hm.js?07e176c4b9cf0ada5d5ebb6915f2f563";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    </script>
 </head>
 <?php echo $content; ?>
 
